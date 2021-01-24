de55c66da2cd486e8e0f367389b4cb37dd123dff
Abdullah Mughal <77934082+abdullahmughaal@users.noreply.github.com>
2021-01-24 23:05:26 +0500

    Add Student functionality

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" exac to="/">
+          Web Technology II Semester Project
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <Link className="btn btn-outline-light" to="/users/add">Add Student</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
