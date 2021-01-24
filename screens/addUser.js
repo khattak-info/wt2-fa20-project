20550642d83371cf383c331f1beb257186a513c9
gitasad <amanzoor.bscs17seecs@seecs.edu.pk>
2021-01-24 21:06:12 +0500

    added add user functionality by Asad Manzoor

--- screens/addUser.js
+++ screens/addUser.js
@@ -1,16 +1,128 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Button, StyleSheet, TextInput, ScrollView, ActivityIndicator, View } from 'react-native';
 import firebase from '../FirebaseConfig';
 
 class AddUserScreen extends Component {
-  
+  constructor() {
+    super();
+    this.dbRef = firebase.firestore().collection('users');
+    this.state = {
+      name: '',
+      cmsid: '',
+      githublink: '',
+      imagelink: '',
+      isLoading: false
+    };
+  }
+
+  inputValueUpdate = (val, prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+  }
+
+  storeUser() {
+    if(this.state.name === ''){
+     alert('Fill at least your name!')
+    } else {
+      this.setState({
+        isLoading: true,
+      });      
+      this.dbRef.add({
+        name: this.state.name,
+        cmsid: this.state.cmsid,
+        githublink: this.state.githublink,
+        imagelink: this.state.imagelink,
+      }).then((res) => {
+        this.setState({
+          name: '',
+          cmsid: '',
+          githublink: '',
+          imagelink: '',
+          isLoading: false,
+        });
+        this.props.navigation.navigate('UserScreen')
+      })
+      .catch((err) => {
+        console.error("Error found: ", err);
+        this.setState({
+          isLoading: false,
+        });
+      });
+    }
+  }
 
   render() {
+    if(this.state.isLoading){
+      return(
+        <View style={styles.preloader}>
+          <ActivityIndicator size="large" color="#9E9E9E"/>
+        </View>
+      )
+    }
     return (
-      
+      <ScrollView style={styles.container}>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'Name'}
+              value={this.state.name}
+              onChangeText={(val) => this.inputValueUpdate(val, 'name')}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'Image Link'}
+              value={this.state.imagelink}
+              onChangeText={(val) => this.inputValueUpdate(val, 'imagelink')}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'CMS ID'}
+              value={this.state.cmsid}
+              onChangeText={(val) => this.inputValueUpdate(val, 'cmsid')}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'Github Link'}
+              value={this.state.githublink}
+              onChangeText={(val) => this.inputValueUpdate(val, 'githublink')}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title='Add User'
+            onPress={() => this.storeUser()} 
+            color="#19AC52"
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35
+  },
+  inputGroup: {
+    flex: 1,
+    padding: 0,
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  preloader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default AddUserScreen;
