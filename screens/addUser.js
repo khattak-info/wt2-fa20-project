c578568c027cfdfdda5fbeefdc081203cbc2ab36
Awanite <awais8adnan@gmail.com>
2021-01-24 20:42:32 +0500

    Added User lists and User Detail by Muhammad Awais Adnan

---
+++ screens/addUser.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from '../FirebaseConfig';
+
+class AddUserScreen extends Component {
+  
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+
+export default AddUserScreen;
---
+++ screens/update.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from '../FirebaseConfig';
+
+class UpdateScreen extends Component {
+
+  
+
+  render() {
+   
+    return (
+      
+    );
+  }
+}
+
+
+
+export default UpdateScreen;
