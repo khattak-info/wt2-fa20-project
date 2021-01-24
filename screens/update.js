a82a19206a1f447911d23e6f2e79754e17a3c47a
zaynmalik2611 <zainulabideen.zm@gmail.com>
2021-01-24 20:56:14 +0500

    Update update.js

--- screens/update.js
+++ screens/update.js
@@ -1,19 +1,164 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Alert, Button, StyleSheet, TextInput, ScrollView, ActivityIndicator, View } from 'react-native';
 import firebase from '../FirebaseConfig';
 
 class UpdateScreen extends Component {
 
-  
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      cmsid: '',
+      githublink: '',
+      imagelink: '',
+      isLoading: true
+    };
+  }
+ 
+  componentDidMount() {
+    const dbRef = firebase.firestore().collection('users').doc(this.props.route.params.userkey)
+    dbRef.get().then((res) => {
+      if (res.exists) {
+        const user = res.data();
+        this.setState({
+          key: res.id,
+          name: user.name,
+          cmsid: user.cmsid,
+          githublink: user.githublink,
+          imagelink: user.imagelink,
+          isLoading: false
+        });
+      } else {
+        console.log("Document does not exist!");
+      }
+    });
+  }
+
+  inputValueUpdate = (val, prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+  }
+
+  updateUser() {
+    this.setState({
+      isLoading: true,
+    });
+    const updateDBRef = firebase.firestore().collection('users').doc(this.state.key);
+    updateDBRef.set({
+      name: this.state.name,
+      cmsid: this.state.cmsid,
+      githublink: this.state.githublink,
+      imagelink: this.state.imagelink,
+    }).then((docRef) => {
+      this.setState({
+        key: '',
+        name: '',
+        cmsid: '',
+        githublink: '',
+        imagelink: '',
+        isLoading: false,
+      });
+      this.props.navigation.navigate('UserScreen');
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+      this.setState({
+        isLoading: false,
+      });
+    });
+  }
+
+  deleteUser() {
+    const dbRef = firebase.firestore().collection('users').doc(this.props.route.params.userkey)
+      dbRef.delete().then((res) => {
+          console.log('Item removed from database')
+          this.props.navigation.navigate('UserScreen');
+      })
+  }
+
 
   render() {
-   
+    if(this.state.isLoading){
+      return(
+        <View style={styles.preloader}>
+          <ActivityIndicator size="large" color="#9E9E9E"/>
+        </View>
+      )
+    }
     return (
-      
+      <ScrollView style={styles.container}>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'Name'}
+              value={this.state.name}
+              onChangeText={(val) => this.inputValueUpdate(val, 'name')}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'Image Link'}
+              value={this.state.imagelink}
+              onChangeText={(val) => this.inputValueUpdate(val, 'imagelink')}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'CMS ID'}
+              value={this.state.cmsid}
+              onChangeText={(val) => this.inputValueUpdate(val, 'cmsid')}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'Github Link'}
+              value={this.state.githublink}
+              onChangeText={(val) => this.inputValueUpdate(val, 'githublink')}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title='Update'
+            onPress={() => this.updateUser()} 
+            color="#19AC52"
+          />
+          </View>
+         <View>
+          <Button
+            title='Delete'
+            onPress={() => this.deleteUser()}
+            color="#E37399"
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35
+  },
+  inputGroup: {
+    flex: 1,
+    padding: 0,
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  preloader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  button: {
+    marginBottom: 7, 
+  }
+})
 
 export default UpdateScreen;
